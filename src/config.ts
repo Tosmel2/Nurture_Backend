2cb5c79e2bad217a39e6991171ca2d5d476edd55
tosmel2 <adewaletosin0808@gmail.com>
2025-12-09 17:33:54 +0100

    feat: changes made for development

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '7d';
-export const JWT_SECRET = process.env.JWT_SECRET || 'please-change-this-secret';
+export const JWT_SECRET = process.env.JWT_SECRET as string;
 export const MONGO_URI = process.env.MONGO_URI || '';
+
+if (!JWT_SECRET) {
+  throw new Error('JWT_SECRET is not defined in environment variables');
+}
